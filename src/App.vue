87dcd3a4f84d51a95918828615af381aad284924
Farida Atei <faridaabdalla0@gmail.com>
2018-06-22 15:00:29 +0300

    added repo

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
+    <h3>{{ title }}  </h3>
     <form>
-      <textarea placeholder="Add new task" v-model="newTask"></textarea>
-      <p>{{newTask}}</p>
+      <textarea placeholder="Add new task" v-model=
+      "newTask"></textarea>
+      <P>{{newTask}}</P>
 
-      <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
-
+      <button type="button" v-on:click="addTask">Add task {{tasks.length}}</button>
     </form>
+
     <ul>
-      <li v-for="task in tasks">{{task}}</li>
+    <li v-for="task in tasks">{{ task }} 
+  </li>
+
     </ul>
   </div>
 </template>
 
 <script>
-
 export default {
   data: function() {
     return {
-      title: 'My Task Manager',
-      newTask: 'This is a SAMPLE ',
-      tasks: []
+      title: 'my task manager',
+      newTask:'This is farida',
+      clicked: 0,
+      tasks: [
+       
+      ]
     }
   },
   methods: {
     addTask() {
-      this.tasks.unshift(this.newTask);
-      this.newTask = '';
+      this.tasks.unshift(this.newTask)
+      this.newTask='';
+
     }
   }
 }
